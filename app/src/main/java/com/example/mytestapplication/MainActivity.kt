b600548a4098f93268814fbc81dbe9b566971fdb
SARAVAN KUMAR MS <skumar126@gmail.com>
2024-01-20 14:44:58 +0530

    Added new files to New  branch

--- app/src/main/java/com/example/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplication/MainActivity.kt
@@ -15,32 +15,8 @@ import com.example.mytestapplication.ui.theme.MyTestApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyTestApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyTestApplicationTheme {
-        Greeting("Android")
-    }
-}
