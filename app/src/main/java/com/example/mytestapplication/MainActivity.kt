9ce1afc0f60ba96d0271d68445dea4964ec290a4
SaravanKumar <skumar126@gmail.com>
2024-02-06 17:12:00 +0530

    asdas

--- app/src/main/java/com/example/mytestapplication/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplication/MainActivity.kt
@@ -2,16 +2,7 @@ package com.example.mytestapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mytestapplication.databinding.LayoutActivityMainBinding
-import com.example.mytestapplication.ui.theme.MyTestApplicationTheme
 
 class MainActivity : ComponentActivity() {
 
